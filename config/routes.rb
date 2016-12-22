1f3e341a3bf29d997ed3c96a702a0d99ac0bebfc
Hiroomi Kimura <643.cheesecake@gmail.com>
2016-12-22 12:24:46 +0000

    create message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   root 'messages#index'
+  # URI: /
+  # HTTPメソッド：GET
+  # コントローラ名 messages
+  # アクション名 index
+  # get 'messages/index' #, to: 'messages#index'
+  # URI: /messages/index
+  # HTTPメソッド：GET
+  # コントローラ名 messages
+  # アクション名 index
+ resources :messages , only: [:create]
+# post 'messages', to: 'messages/create'
+# index /messages get
+# show
+# new
+# edit
+# create /messages post 
+# update
+# destroy
 end
